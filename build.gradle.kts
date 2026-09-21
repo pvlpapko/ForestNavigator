8525a5b866ff6f1542f6c4af41bb660942a5817c
Pavel <pvlpapko@gmail.com>
2026-09-21 21:04:58 +0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    }
+}
+
+plugins {
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+}
