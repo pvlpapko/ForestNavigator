253b59a784152d8520d168c8cf86023628460fa2
Pavel <pvlpapko@gmail.com>
2026-09-21 21:14:12 +0300

    Use stable Android 16 SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "app.forestnav"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "app.forestnav"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
 
