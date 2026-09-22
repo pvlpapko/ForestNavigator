49058867b13ea04a06650011bffbcd845cda67e6
Pavel <pvlpapko@gmail.com>
2026-09-22 15:21:34 +0300

    Bump Forest Navigator to 0.9.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.forestnav"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.9.9"
+        versionCode = 19
+        versionName = "0.9.10"
         buildConfigField("String", "MAPTILER_KEY", "\"il3KGdrwRY1RGiUuAlLr\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
