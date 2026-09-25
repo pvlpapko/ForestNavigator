988c433ad8145d725db72069d205b7c8f8c08607
Pavel <pvlpapko@gmail.com>
2026-09-25 22:27:41 +0300

    Use MapLibre HttpRequestImpl client hook

--- app/src/main/java/app/forestnav/ForestNavApplication.kt
+++ app/src/main/java/app/forestnav/ForestNavApplication.kt
@@ -34,7 +34,7 @@ class ForestNavApplication : Application() {
             .retryOnConnectionFailure(true)
             .build()
 
-        HttpRequestUtil.setOkHttpClient(mapHttpClient)
+        HttpRequestImpl.setOkHttpClient(mapHttpClient)
 
         database = ForestDatabase(this)
         settings = SettingsStore(this)
